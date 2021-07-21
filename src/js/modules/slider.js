01ca3aca50666b5a01af3c646e3e4d519ce02e83
Irina <diva84@mail.ru>
2021-07-21 05:50:50 +0300

    decor

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -4,14 +4,13 @@ SwiperCore.use([Navigation, Pagination]);
 // import Swiper from 'swiper/bundle'; (подключение со всеми свойствами)
 
 const slider = () => {
-	const mainSlider = new Swiper('.swiper-container', {
+	const mainSlider = new Swiper('.glazing-slider', {
 
 		breakpoints: {
-			993: {
-				watchOverflow: true,
-				slidesPerView: 5,
-				loop: false,
-			},
+			// 993: {
+			// 	watchOverflow: true,
+			// 	slidesPerView: 5,
+			// },
 			769: {
 				slidesPerView: 4,
 			},
@@ -21,10 +20,29 @@ const slider = () => {
 		},
 
 		navigation: {
-			nextEl: '.next',
-			prevEl: '.prev',
+			nextEl: '.glazing-slider__prev',
+			prevEl: '.glazing-slider__next',
 		},
 
+
+		grabCursor: true,
+		slidesPerGroup: 1,
+		initialSlide: 0,
+		slidesPerView: 2,
+		speed: 800,
+	});
+
+	const secondSlider = new Swiper('.decoration-slider', {
+
+		breakpoints: {
+			577: {
+				watchOverflow: true,
+				slidesPerView: 4,
+			},
+			440: {
+				slidesPerView: 3,
+			},
+		},
 
 		grabCursor: true,
 		slidesPerGroup: 1,
